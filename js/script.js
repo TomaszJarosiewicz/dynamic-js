a102403c40560ed1ae91d8dd49601a03998b4bc3
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-07-23 10:10:49 +0200

    Add loop for remove class

--- js/script.js
+++ js/script.js
@@ -9,7 +9,19 @@ function titleClickHandler(event){
 
     /* remove class 'active' from all articles */
 
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+
     /* get 'href' attribute from the clicked link */
+
+    const activeArticles = document.querySelectorAll('.post');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
 
     /* find the correct article using the selector (value of 'href' attribute) */
 

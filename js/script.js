359a4910d34200348cbdc8554f8e647d8501ef16
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-07-23 10:24:14 +0200

    Add support 'this'

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 'use strict';
 
 function titleClickHandler(event){
+    const clickedElement = this;
     console.log(event);
 
     /* remove class 'active' from all article links  */
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    console.log('clickedElement:', clickedElement);
+    /* [DONE] remove class 'active' from all articles */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
+        clickedElement.classList.add('active');
     }
 
     /* get 'href' attribute from the clicked link */
